0c8388633770007ac821bcd0fb2743c8fac02d91
colman <colman@ubuntu>
2015-07-17 04:38:30 -0700

    ds

--- Robotics/src/Movement/LocalizationManager.h
+++ Robotics/src/Movement/LocalizationManager.h
@@ -1,32 +1,32 @@
-/*
- * LocalizationManager.h
- *
- *  Created on: Jul 15, 2015
- *      Author: colman
- */
-
-#ifndef LOCALIZATIONMANAGER_H_
-#define LOCALIZATIONMANAGER_H_
-
-#include "Particle.h"
-#include "../Utils/GridMap.h"
-#include <vector>
-
-using namespace std;
-
-// TODO - maybe do singleton
-class LocalizationManager {
-	GridMap grid;
-	LocalizationManager(GridMap gridMap, double yawRobot);
-//	LocalizationManager(LocalizationManager const&);   // Don't Implement.
-//		        void operator=(LocalizationManager const&); // Don't implement
-public:
-//    static LocalizationManager* getInstance();
-	static const int PARTICLE_NUM = 50;
-	std::vector<Particle> particles;
-
-	virtual ~LocalizationManager();
-};
-
-
-#endif /* LOCALIZATIONMANAGER_H_ */
+///*
+// * LocalizationManager.h
+// *
+// *  Created on: Jul 15, 2015
+// *      Author: colman
+// */
+//
+//#ifndef LOCALIZATIONMANAGER_H_
+//#define LOCALIZATIONMANAGER_H_
+//
+//#include "Particle.h"
+//#include "../Utils/GridMap.h"
+//#include <vector>
+//
+//using namespace std;
+//
+//// TODO - maybe do singleton
+//class LocalizationManager {
+//	GridMap grid;
+//	LocalizationManager(GridMap gridMap, double yawRobot);
+////	LocalizationManager(LocalizationManager const&);   // Don't Implement.
+////		        void operator=(LocalizationManager const&); // Don't implement
+//public:
+////    static LocalizationManager* getInstance();
+//	static const int PARTICLE_NUM = 50;
+//	std::vector<Particle> particles;
+//
+//	virtual ~LocalizationManager();
+//};
+//
+//
+//#endif /* LOCALIZATIONMANAGER_H_ */
